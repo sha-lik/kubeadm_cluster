ae3e05953d39d7ade6d2019f75673b2fa3339bd3
sha-lik <shankar.s@ramanujan.du.ac.in>
2022-06-21 14:09:36 +0530

    hi

---
+++ kubectl_install.sh
@@ -0,0 +1,12 @@
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+
+sudo apt list kubeadm
+sudo apt list kubeadm -a
+sudo apt-get install -y kubelet=1.21.14-00 kubeadm=1.21.14-00 kubectl=1.21.14-00
+kubectl version
+kubeadm version 
+kubelet --version
+
