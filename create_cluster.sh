9acd85bd7a8010bb191e2ef2ccddb2cdb86df330
sha-lik <shankar.s@ramanujan.du.ac.in>
2022-06-21 12:38:08 +0530

    hi

---
+++ create_cluster.sh
@@ -0,0 +1,11 @@
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16
+
+
+mkdir -p $HOME/.kube
+  sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+  sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+
+kubectl apply -f https://raw.githubusercontent.com/flannel-io/flannel/master/Documentation/kube-flannel.yml
+kubectl taint nodes --all  node-role.kubernetes.io/master-
+
